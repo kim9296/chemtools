728a7d8e89ca364c51270ac22b36b6e0140d48d0
HJ Kim <kim9296@naver.com>
2022-05-18 17:24:07 +0900

    change ligand docking

--- ligand_docking.py
+++ ligand_docking.py
@@ -4,34 +4,38 @@ from preprocessing_inhibition import fill_smiles
 import os
 
 parser = ArgumentParser(description='ligand docking')
-parser.add_argument('--csv', metavar='CSV', help='input csv', dest='csv', required=True)
+parser.add_argument('--inp', help='input files (csv, sdf)', dest='inp', required=True)
 parser.add_argument('--ref', help='smiles csv file', default=None, type=str)
 parser.add_argument('--grid', metavar='grid', help='docking grid', dest='grid', required=True)
 
 if __name__ == '__main__':
     args = parser.parse_args()
     
-    csv_name = os.path.abspath(args.csv)
+    csv_name = os.path.abspath(args.inp)
     if args.ref is not None:
     	ref_name = os.path.abspath(args.ref)
     out_name = csv_name.replace('raw', 'result')
     grid_name = os.path.abspath(args.grid)
     dir_name = os.path.dirname(out_name)
-    sdf_name = out_name.replace('.csv', '.sdf')
+    if 'sdf' in csv_name:
+	sdf_name = out_name
+    else:
+        sdf_name = out_name.replace('.csv', '.sdf')
     grid_base = os.path.basename(grid_name)
     ligand_name = sdf_name.replace('.sdf', '.maegz')
     docking_in = sdf_name.replace('.sdf', '_{}.in'.format(grid_base.split('.zip')[0]))
     docking_out = sdf_name.replace('.sdf', '_pv.maegz')    
     
     if not os.path.exists(ligand_name):
-        #if args.ref is not None:
-        #    print ('preprocess csv')
-        #    fill_smiles(csv_name, ref_name)
-        #else:
-        #os.system('cp {} {}'.format(csv_name, out_name))
+        if args.ref is not None:
+            print ('preprocess csv')
+            fill_smiles(csv_name, ref_name)
+        else:
+            os.system('cp {} {}'.format(csv_name, out_name))
         
-        #print ('convert sdf')
-        #csv2sdf(out_name, sdf_name)
+        if 'sdf' not in csv_name:
+            print ('convert sdf')
+            csv2sdf(out_name, sdf_name)
     
         os.chdir(dir_name)   
     
