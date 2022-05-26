192cbcd493bc611de05b7fafa10333569f4254e5
HJ Kim <kim9296@naver.com>
2022-05-26 14:12:34 +0900

    fix postdocking

--- post_docking.py
+++ post_docking.py
@@ -37,6 +37,9 @@ def make_dataset(csv, xcol, res, ycol=None):
     if len(ycols) == 2:
         df[ycol] = np.where(pd.notnull(df[ycols[0]]) == True, df[ycols[0]], df[ycols[1]])
     elif len(ycols) > 2:
+        df[ycol] = np.where(pd.notnull(df[ycols[0]]) == True, df[ycols[0]], df[ycols[1]])
+        for i in range(1, len(ycols)):
+            df[ycol] = np.where(pd.notnull(df[ycol]) == True, df[ycol], df[ycols[i]])
         raise ('too many ycols, check IFP.csv')
     else:
         df[ycol] = df[ycols[0]]
