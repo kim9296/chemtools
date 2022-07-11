b3228e04bad4fc2d9f5b0f746eadd3bce6dbe77d
HJ <kim9296@naver.com>
2022-07-11 11:03:53 +0900

    add get pdb and proteinprep

---
+++ get_pdb_and_prep.py
@@ -0,0 +1,19 @@
+from argparse import ArgumentParser
+import os
+
+parser = ArgumentParser(description='Get PDB and run prepwizard')
+parser.add_argument('--inp', help='PDB code', dest='inp', nargs='+', required=True)
+
+def GetPDBandPrep(pdb_code):
+    os.system('$SCHRODINGER/utilities/getpdb {}'.format(pdb_code))
+    os.makedirs('proteinprep_{}'.format(pdb_code))
+    os.system('mv {}.pdb proteinprep_{}'.format(pdb_code, pdb_code))
+    os.chdir('proteinprep_{}'.format(pdb_code))
+    os.system('$SCHRODINGER/utilities/prepwizard {}.pdb proteinprep_{}.maegz'.format(pdb_code, pdb_code))
+    os.chdir('..')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    for pdb in args.inp:
+        GetPDBandPrep(pdb)
+    print ('finish')
